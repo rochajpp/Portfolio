c64ee0f283f331410a57cc0dee629495a04852f6
João Paulo Medeiros Rocha <joaopauloadm@7setembro.com.br>
2023-01-23 12:25:22 -0300

    Finalizandoparte de apresentação e iniciando parte de botoes

--- script/script.js
+++ script/script.js
@@ -11,7 +11,16 @@ document.querySelector('.language').onclick = function(){
     document.querySelector('body').classList.toggle('active');
 }
 
-function writerEffect(){
-
+function typeWriter(element){
+    const textArray = element.innerHTML.split('')
+    element.innerHTML = ''
+    setTimeout(function(){
+        textArray.forEach((letra, i) => { //Para cada elemento um index
+            setTimeout(function(){
+                element.innerHTML += letra
+            }, 75 * i)
+        })
+    }, 2000)
 }
-writerEffect();
+
+typeWriter(document.querySelector('#writer'));
