21422b9df4e6bdb8276dd40f730e08478cbec30b
João Paulo Medeiros Rocha <joaopauloadm@7setembro.com.br>
2023-01-24 13:50:33 -0300

    Finalizando menu responsivo

--- script/script.js
+++ script/script.js
@@ -12,6 +12,18 @@ document.querySelector('.menuButton').onclick = function(){
 document.querySelector('.close').onclick = function(){
     document.querySelector('.contentMain').classList.toggle('menuRespActive');
 }
+document.querySelector('.homeA').onclick = function(){
+    document.querySelector('.contentMain').classList.toggle('menuRespActive');
+}
+document.querySelector('.aboutA').onclick = function(){
+    document.querySelector('.contentMain').classList.toggle('menuRespActive');
+}
+document.querySelector('.projectsA').onclick = function(){
+    document.querySelector('.contentMain').classList.toggle('menuRespActive');
+}
+document.querySelector('.contactsA').onclick = function(){
+    document.querySelector('.contentMain').classList.toggle('menuRespActive');
+}
 document.querySelector('.language').onclick = function(){
     document.querySelector('body').classList.toggle('active');
 }
