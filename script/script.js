43b0021194a2b31dc9e1aa5e7c37deef28f035af
João Paulo Medeiros Rocha <joaopauloadm@7setembro.com.br>
2023-01-24 12:38:08 -0300

    About feito ajustes na responsividade feitas, icone adicionado

--- script/script.js
+++ script/script.js
@@ -24,3 +24,12 @@ function typeWriter(element){
 }
 
 typeWriter(document.querySelector('#writer'));
+
+
+window.scrl = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '20px' });
+
+scrl.reveal('.apresentation', {duration: 3000});
+scrl.reveal('.part2', {duration: 3000});
+scrl.reveal('.part1', {duration: 3000, origin: 'left'});
+scrl.reveal('.titleAbout', {duration: 3000});
