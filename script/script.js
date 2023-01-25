079800a321d97d185fae7ecc7560ab0618fce8ec
João <joao.rochap03@gmail.com>
2023-01-24 21:19:44 -0300

    Organizando scripts e iniciando #projects

--- script/script.js
+++ script/script.js
@@ -6,27 +6,6 @@ function activeHeaderScroll(){
 
 window.addEventListener('scroll', activeHeaderScroll);
 
-document.querySelector('.menuButton').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.close').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.homeA').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.aboutA').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.projectsA').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.contactsA').onclick = function(){
-    document.querySelector('.contentMain').classList.toggle('menuRespActive');
-}
-document.querySelector('.language').onclick = function(){
-    document.querySelector('body').classList.toggle('active');
-}
 
 function typeWriter(element){
     const textArray = element.innerHTML.split('')
@@ -43,10 +22,3 @@ function typeWriter(element){
 typeWriter(document.querySelector('#writer'));
 
 
-window.scrl = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '20px' });
-
-scrl.reveal('.apresentation', {duration: 3000});
-scrl.reveal('.part2', {duration: 3000});
-scrl.reveal('.part1', {duration: 3000, origin: 'left'});
-scrl.reveal('.titleAbout', {duration: 3000});
---
+++ script/scrollReveal.js
@@ -0,0 +1,7 @@
+window.scrl = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '20px' });
+
+scrl.reveal('.apresentation', {duration: 3000});
+scrl.reveal('.part2', {duration: 3000});
+scrl.reveal('.part1', {duration: 3000, origin: 'left'});
+scrl.reveal('.titleAbout', {duration: 3000});
