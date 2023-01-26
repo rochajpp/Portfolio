5c7bc6a12ca634823e840aa8def706b80d0f2bd1
João <joao.rochap03@gmail.com>
2023-01-25 22:30:27 -0300

    Refazendo #about me

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -2,7 +2,5 @@ window.scrl = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '20px' });
 
 scrl.reveal('.apresentation', {duration: 3000});
-scrl.reveal('.part2', {duration: 3000});
-scrl.reveal('.part1', {duration: 3000, origin: 'left'});
-scrl.reveal('.titleAbout', {duration: 3000});
+
 scrl.reveal('.projects', {duration: 3000});
