a0e8691610f39dc4c19a61698fa1c431877af1b1
João <joao.rochap03@gmail.com>
2023-01-25 21:22:12 -0300

    Novo hover adicionado os botoes de #home

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -5,3 +5,4 @@ scrl.reveal('.apresentation', {duration: 3000});
 scrl.reveal('.part2', {duration: 3000});
 scrl.reveal('.part1', {duration: 3000, origin: 'left'});
 scrl.reveal('.titleAbout', {duration: 3000});
+scrl.reveal('.projects', {duration: 3000});
