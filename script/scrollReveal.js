153d5e3b3a832ca58b4fe682b2d8819ba59e29cd
João Paulo Medeiros Rocha <joaopauloadm@7setembro.com.br>
2023-01-30 12:58:05 -0300

    Cor de campos do formulário de contatos alterado e resposividade de formulário feita

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -1,10 +1,10 @@
-window.scrl = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '20px' });
+// window.scrl = ScrollReveal({ reset:true});
+// ScrollReveal({ distance: '20px' });
 
-scrl.reveal('.apresentation', {duration: 3000});
+// scrl.reveal('.apresentation', {duration: 3000});
 
-scrl.reveal('.projects', {duration: 3000});
+// scrl.reveal('.projects', {duration: 3000});
 
-scrl.reveal('.infos', {duration: 3000, origin: 'left'});
+// scrl.reveal('.infos', {duration: 3000, origin: 'left'});
 
-scrl.reveal('.aboutMe', {duration: 3000, origin: 'right'})
+// scrl.reveal('.aboutMe', {duration: 3000, origin: 'right'});
