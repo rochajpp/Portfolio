260608afa5ec61cc85d1535343d2dde907763e1b
João <joao.rochap03@gmail.com>
2023-01-29 21:03:15 -0300

    Começo da section #contact

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -4,3 +4,7 @@ ScrollReveal({ distance: '20px' });
 scrl.reveal('.apresentation', {duration: 3000});
 
 scrl.reveal('.projects', {duration: 3000});
+
+scrl.reveal('.infos', {duration: 3000, origin: 'left'});
+
+scrl.reveal('.aboutMe', {duration: 3000, origin: 'right'})
